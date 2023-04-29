f97f0bb9e46e1088307689e85e08ac878941a45a
Yakub Ojo <yakubojodev@gmail.com>
2023-04-29 11:46:42 +0100

    Fix linters error

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-                                       // SPEAKER SECTION!!!
+// SPEAKER SECTION!!!
 const speakers = [
   {
-      image : 'assets/speaker_01.png',
-      title : 'Resource Person',
-      person : 'Prof. Afolabi Tonye',
-      post : 'Conference Chair & Keynote Speaker',
-      add : 'Former Dean of the Faculty of Agricultural Technology, Al-Balqa Applied University, Jordan.'
+    image: 'assets/speaker_01.png',
+    title: 'Resource Person',
+    person: 'Prof. Afolabi Tonye',
+    post: 'Conference Chair & Keynote Speaker',
+    add: 'Former Dean of the Faculty of Agricultural Technology, Al-Balqa Applied University, Jordan.',
   },
   {
-      image : 'assets/speaker02.png',
-      title : 'Resource Person',
-      person : 'SohYeong Noh',
-      post : 'Conference Chair',
-      add : 'Director of Art Centre Nabi and a board member of CC Korea.'
+    image: 'assets/speaker02.png',
+    title: 'Resource Person',
+    person: 'SohYeong Noh',
+    post: 'Conference Chair',
+    add: 'Director of Art Centre Nabi and a board member of CC Korea.',
   },
   {
-      image : 'assets/speaker03.png',
-      title : 'Resource Person',
-      person : 'Lila Tretikov',
-      post : 'Keynote Speaker',
-      add : 'Former Dean of the Faculty of Agricultural Technology, Al-Balqa Applied University, Jordan.'
+    image: 'assets/speaker03.png',
+    title: 'Resource Person',
+    person: 'Lila Tretikov',
+    post: 'Keynote Speaker',
+    add: 'Former Dean of the Faculty of Agricultural Technology, Al-Balqa Applied University, Jordan.',
   },
   {
-      image : 'assets/speaker04.png',
-      title : 'Resource Person',
-      person : 'Prof. Kilman Chon',
-      post : 'Keynote Speaker',
-      add : 'Dean of Faculty of Agro Based Industry Universiti Malaysia Kelantan Malaysia'
+    image: 'assets/speaker04.png',
+    title: 'Resource Person',
+    person: 'Prof. Kilman Chon',
+    post: 'Keynote Speaker',
+    add: 'Dean of Faculty of Agro Based Industry Universiti Malaysia Kelantan Malaysia',
   },
   {
-      image : 'assets/speaker05.png',
-      title : 'Resource Person',
-      person : 'Julia Leda',
-      post : 'Plenary Speaker',
-      add : 'President of Young Pirates of Europe'
+    image: 'assets/speaker05.png',
+    title: 'Resource Person',
+    person: 'Julia Leda',
+    post: 'Plenary Speaker',
+    add: 'President of Young Pirates of Europe',
   },
   {
-      image : 'assets/speaker06.png',
-      title : 'Resource Person',
-      person : 'Ryan Dyrud',
-      post : 'Plenary Speaker',
-      add : 'Berkman Professor of Enterpreneurial Legal Studies'
+    image: 'assets/speaker06.png',
+    title: 'Resource Person',
+    person: 'Ryan Dyrud',
+    post: 'Plenary Speaker',
+    add: 'Berkman Professor of Enterpreneurial Legal Studies',
   },
-]
+];
 
 const resource = document.querySelector('#speaker-card');
 
 for (let i = 0; i < speakers.length; i += 1) {
-resource.innerHTML += `
+  resource.innerHTML += `
       <div class="speaker-cont ">
         <div class="speaker-img">
             <img src="${speakers[i].image}" alt="speaker1-img">
@@ -65,40 +65,34 @@ resource.innerHTML += `
   `;
 }
 
-                           // MOBILE MENU-LIST (HAMBURGER)
-
+// MOBILE MENU-LIST (HAMBURGER)
 
 const menuIcon = document.querySelector('.hamburger');
 const closeIcon = document.querySelector('.closeIcon');
 const overlay = document.querySelector('.overlay');
 const menu = document.querySelector('.menu-links');
-const menuList = document.querySelectorAll('.menu-links a');
 const { body } = document;
 
 const displayMenu = () => {
   menuIcon.style.display = 'none';
   overlay.classList.remove('hidden');
   menu.style.display = 'flex';
-  closeIcon.style.display = 'block'
+  closeIcon.style.display = 'block';
   body.style.overflow = 'hidden';
-}
+};
 
 const closeMenu = () => {
   menuIcon.style.display = 'block';
   overlay.classList.add('hidden');
   menu.style.display = 'none';
-  closeIcon.style.display = 'none'
+  closeIcon.style.display = 'none';
   body.style.overflow = 'scroll';
-}
+};
 
 menuIcon.addEventListener('click', () => {
   displayMenu();
-} )
+});
 
 closeIcon.addEventListener('click', () => {
   closeMenu();
-})
-
-
-
-
+});
